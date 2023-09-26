e94feaa455ee5dfc09ddb9084868e4ba9989e8b4
KrisJovero <125135276+KrisJovero@users.noreply.github.com>
2023-09-27 05:57:09 +0800

    variables.php
    This is a tutorial for github and variable php

---
+++ variables.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title> Variable Types in PHP</title>
+</head>
+<body>
+  
+    <?php
+        $int_var=09090909;
+        echo $int_var
+
+    ?>
+
+</body>
+</html>
