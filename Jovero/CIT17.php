5fb10f811ed5f7a64a6bc8e535b23a661b166cac
KrisJovero <125135276+KrisJovero@users.noreply.github.com>
2023-09-27 13:09:36 +0800

    Add files via upload

---
+++ Jovero/CIT17.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title> Variable Types in PHP</title>
+</head>
+<body>
+  
+    <?php
+        $int_var= 12345;
+        $another_int = -12345 + 12345;
+        echo $another_int
+
+    ?>
+
+</body>
+</html>
---
+++ Jovero/NULL.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title> Variable Types in PHP</title>
+</head>
+<body>
+  
+    <?php
+$my_var = NULL;
+
+
+
+
+    ?>
+
+</body>
+</html>
