5bb2a190f98ac108442c5ea7dc7e01d0c45c500f
KrisJovero <125135276+KrisJovero@users.noreply.github.com>
2023-09-27 13:52:39 +0800

    Chapter4.php
    updated

--- Jovero/Chapter4.php
+++ Jovero/Chapter4.php
@@ -1,28 +1,31 @@
 <?php
 
+// integers
 $int_var = 12345;
 $another_int = -12345 + 12345;
 echo $another_int;
 
-        $many = 2.2888800;
-        $many_2 = 2.2111200;
-        $few = $many + $many_2;
-        print($many + $many_2 = $few);
+//This is Doubles
+$many = 2.2888800;
+$many_2 = 2.2111200;
+$few = $many + $many_2;
+print(.$many + $many_2 = $few<br>.);
 
-        if (TRUE)
-        print("This will always print<br>");
-       else
-        print("This will never print<br>");
-        $true_num = 3 + 0.14159;
+// Code for Boolean
+if (TRUE)
+print("This will always print<br>");
+else
+print("This will never print<br>");
+$true_num = 3 + 0.14159;
 $true_str = "Tried and true";
 $true_array[49] = "An array element";
 $false_array = array();
 $false_null = NULL;
 $false_num = 999 - 999;
 $false_str = "";
-
+//Null
 $my_var = NULL;
-
+//Strings
 $string_1 = "This is a string in double quotes";
 $string_2 = "This is a somewhat longer, singly quoted string";
 $string_39 = "This string has thirty-nine characters";
@@ -33,7 +36,7 @@ $literally = 'My $variable will not print!\\n';
 print($literally);
 $literally = "My $variable will print!\\n";
 print($literally);
-
+//Here Document
 $channel =<<<_XML_
 <channel>
 <title>What's For Dinner<title>
@@ -49,23 +52,24 @@ line with just a semicolon. no extra whitespace!
 <br />
 END;
 print $channel;
-
+// local variables
 $x = 4;
 function assignx () {
-    $x = 0;
-    print "\$x inside function is $x. 
-    ";
-    }
-    assignx();
-    print "\$x outside of function is $x. 
-    ";
-    // multiply a value by 10 and return it to the caller
+$x = 0;
+print "\$x inside function is $x. 
+";
+}
+assignx();
+print "\$x outside of function is $x. 
+";
+// multiply a value by 10 and return it to the caller - FUNCTION PARAMETERS
 function multiply ($value) {
     $value = $value * 10;
     return $value;
-   }
-   $retval = multiply (10);
-   Print "Return value is $retval\n";
+}
+$retval = multiply (10);
+Print "Return value is $retval\n";
+//Welcome to Global Variables
    $somevar = 15;
 function addit() {
 GLOBAL $somevar;
@@ -73,6 +77,7 @@ $somevar++;
 print "Somevar is $somevar";
 }
 addit();
+// Static Variables
 function keep_track() {
     STATIC $count = 0;
     $count++;
