7d4df01d2a55ac573fdb4113462fa450352d082a
KrisJovero <125135276+KrisJovero@users.noreply.github.com>
2023-10-09 08:33:03 +0400

    This is an updated Chapter 6

--- Jovero/Chapter 6.php
+++ Jovero/Chapter 6.php
@@ -1,4 +1,26 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head><title>Arithmetical Operators</title><head>
 <body>
 <?php
+    $a = 42;
+    $b = 20;
+
+    $c = $a + $b;
+    echo "Sum : $c </br>";
+    $c = $a - $b;
+    echo "Difference : $c </br>";
+    $c = $a * $b;
+    echo "Product : $c </br>";
+    $c = $a / $b;
+    echo "Quotient : $c </br>";
+    $c = $a % $b;
+    echo "Modulo : $c </br>";
+    $c = $a++;
+    echo "Increment : $c </br>";
+    $c = $a--;
+    echo "Decrement : $c </br>";
+
+?>
+</body>
+</html
