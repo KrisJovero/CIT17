020dd3e75fc4088f70b61064fba2a77dda847913
KrisJovero <natrovez@Gmail.com>
2023-10-18 07:29:56 +0800

    Not yet done, output

--- Jovero/CalculatorHere.php
+++ Jovero/CalculatorHere.php
@@ -2,12 +2,12 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="X-UA-Compatible" content="IE edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Result of Calculation</title>
 </head>
 <body>
 Hi <?php echo htmlspecialchars($_POST['name']); ?>.
-You are <?php echo (int)$_POST['age']; ?> years old.
+You are <?php echo (int)$_POST['age']; ?>.
 </body>
 </html>
