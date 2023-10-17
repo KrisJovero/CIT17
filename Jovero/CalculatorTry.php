289f9085966c58d66e44682f166cb454828a09fa
KrisJovero <natrovez@Gmail.com>
2023-10-18 07:30:28 +0800

    Not yet finish, input-calculate

--- Jovero/CalculatorTry.php
+++ Jovero/CalculatorTry.php
@@ -1,28 +1,59 @@
 <!DOCTYPE html>
 <html lang="en">
-<head><title>Calculator</title><head>
+<head>
+    <title>Calculator</title>
+</head>
 <body>
 <?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $a = $_POST["a"];
+    $b = $_POST["b"];
+    $operation = $_POST["operation"];
+    $result = 0;
 
-// code for asking what operation
-    $a = 42;
-    $b = 20;
+    if ($operation === "addition") {
+        $result = $a + $b;
+    } elseif ($operation === "subtraction") {
+        $result = $a - $b;
+    } elseif ($operation === "multiplication") {
+        $result = $a * $b;
+    } elseif ($operation === "division") {
+        if ($b == 0) {
+            echo "Error: Division by zero is not allowed. <br>";
+        } else {
+            $result = $a / $b;
+        }
+    } elseif ($operation === "modulus") {
+        if ($b == 0) {
+            echo "Error: Modulus by zero is not allowed. <br>";
+        } else {
+            $result = $a % $b;
+        }
+    } else {
+        echo "Invalid operation selected. <br>";
+    }
 
-    $c = $a + $b;
-    echo "Sum : $c </br>";
-    $c = $a - $b;
-    echo "Difference : $c </br>";
-    $c = $a * $b;
-    echo "Product : $c </br>";
-    $c = $a / $b;
-    echo "Quotient : $c </br>";
-    $c = $a % $b;
-    echo "Modulo : $c </br>";
-    $c = $a++;
-    echo "Increment : $c </br>";
-    $c = $a--;
-    echo "Decrement : $c </br>";
-
+    echo "Result: $result <br>";
+}
 ?>
+
+<form method="post" action="">
+    <label for="a">Enter the first number: </label>
+    <input type="number" name="a" id="a" required><br>
+
+    <label for="b">Enter the second number: </label>
+    <input type="number" name="b" id="b" required><br>
+
+    <label for="operation">Select an operation: </label>
+    <select name="operation" id="operation">
+        <option value="addition">Addition</option>
+        <option value="subtraction">Subtraction</option>
+        <option value="multiplication">Multiplication</option>
+        <option value="division">Division</option>
+        <option value="modulus">Modulus</option>
+    </select><br>
+
+    <input type="submit" value="Calculate">
+</form>
 </body>
-</html
+</html>
